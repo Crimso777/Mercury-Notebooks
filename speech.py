7ad977e46426e1656948281d22221b3ccdcaf3b3
Solomon Weatherby <64358250+yosolomxn@users.noreply.github.com>
2022-11-05 22:20:09 -0500

    Add files via upload
    Tested code before submission with final tweaks

---
+++ speech.py
@@ -0,0 +1,35 @@
+import speech_recognition as sr
+import webbrowser as wb
+import os
+
+r1 = sr.Recognizer()
+r2 = sr.Recognizer()
+r3 = sr.Recognizer()
+
+with sr.Microphone () as source:
+    print('[search edureka: search youtube]')
+    print('speak now')
+    audio = r3.listen(source)
+
+if 'edureka' in r2.recognize_google(audio):
+    r2 = sr.Recognizer()
+    url = 'https://www.edureka.co/'
+    with sr.Microphone as source:
+        print('search your query')
+        audio = r2.listen(source)
+
+        try:
+            get = r2.recognize_google(audio)
+            print(get)
+            wb.get().open_new(url+get)
+        except sr.RequestError as e:
+            print('failed'.format(e))
+
+#with sr.AudioFile('SteveJobsSpeech_2min.wav') as source:
+    audio = r.listen(source)
+    try:
+        text = r.recognize+_google(audio)
+        print('Working on...')
+        print(text)
+    except:
+        print('Sorry...run again...')
---
+++ speech2text.py
@@ -0,0 +1,49 @@
+# Run Program: python -m speech_recognition
+import speech_recognition as sr
+import webbrowser as wb
+import pyttsx3
+
+# Initialize the recognizer
+r = sr.Recognizer()
+
+# Function to convert text to speech
+def SpeakText(command):
+
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
+
+# Use the microphone as source for input
+with sr.Microphone() as source2:
+
+    # Wait for the recognizer to adjust the energy threshold based on the background noise
+    r.adjust_for_ambient_noise(source2, duration=0.2)
+
+    # Listens for the user's input
+    audio2 = r.listen(source2)
+
+    # Using google to recognize audio
+    MyText = r.recognize_google(audio2)
+
+    # If the user says "open website" the program will ask what website
+    if MyText == "open website":
+        print("You have said: " + MyText)
+        print("What website would you like?")
+
+        r.adjust_for_ambient_noise(source2, duration=0.2)
+        audio2 = r.listen(source2)
+        MyText = r.recognize_google(audio2)
+
+        # Path to Google Chrome
+        path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
+
+        # Opens desired webiste if it exisits
+        wb.get(path).open(MyText)
+
+    # The program will repeat what you said back
+    else:
+        MyText = MyText.lower()
+
+        print("Did you say " +MyText)
+        SpeakText(MyText)
