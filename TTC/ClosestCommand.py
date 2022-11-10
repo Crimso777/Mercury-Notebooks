d5e5222e9986ebb7212654933f341582813877ab
Jackson Johanek <jack.johanek@gmail.com>
2022-11-10 02:17:37 -0600

    Changed TTC to be a class & updated main
    Changed TTC to be a class, it has two functions so far... Matching
    string to closest command, and finding variables from given string.
    Changed main to use the new class structure, further setting up main to
    be something that can work

--- TTC/ClosestCommand.py
+++ TTC/ClosestCommand.py
@@ -1,4 +1,3 @@
-
 # Take in text from STT
 #assuming input is .txt
 
@@ -9,67 +8,43 @@ nltk.download('punkt')
 from nltk.tokenize import word_tokenize, sent_tokenize
 import re
 
-# Put list of commands from file to code.
-command_file = 'CommandList.txt'
-commands = []
-with open(command_file) as fo:
+class TextToCommands:
+  command_file = 'CommandList.txt'
+  commands = []
+  with open(command_file) as fo:
     tokens = sent_tokenize(fo.read())
     for line in tokens:
         # line = line.replace('\n', ' ')
         line = re.sub(r' \s+', ' ', line)
         commands.append(line)
-commands = re.split("\n", commands[0])
-print('sentences =\n{}'.format(commands))
-
-# This is where we take user input, currently assuming its a text file, but could easily be a single string
-input_file = 'testinput.txt'
-input_sentences = []
-with open(input_file) as f:
-    tokens = sent_tokenize(f.read())
-    for line in tokens:
-        input_sentences.append(line)
-
-''' 
-doing text cleaning because testinput is funky
-'''
-input_sentences = re.split("\n", input_sentences[0])
-input_sentences = [i for i in input_sentences if i != '']
-# If we execute sentence by sentence, we can just append sentences to input_sentences, and work like a queue
-
-print('input_sentence =\n{}'.format(input_sentences))
-
-
-# Model Define
-model = SentenceTransformer('all-MiniLM-L6-v2')
-embeddings = model.encode(commands)
-
-# This executes the matching statements, all we should need to do is map a command in the command file to executing a function in ../Collaborator/{func}
-# We can use the matched command that has 90%+ confidence, if under 90% we can respond back a "command not found"
-print('\nResults:')
-solid_match = []
-for sentence in input_sentences:
-    emb = model.encode(sentence)
-    cos_sim = util.cos_sim(embeddings, [emb])
-
+  commands = re.split("\n", commands[0])
+  print('Commands loaded...\n{}'.format(commands))
+  
+  model = SentenceTransformer('all-MiniLM-L6-v2')
+  embeddings = model.encode(commands)
+  
+  def findMatch(self, input_string):
+    emb = self.model.encode(input_string)
+    cos_sim = util.cos_sim(self.embeddings, [emb])
+    
     sentence_combinations = []
     for i in range(0, len(cos_sim)):
         sentence_combinations.append([i, cos_sim[i]])
+
     sorted_sentence_combinations = sorted(sentence_combinations, key=lambda x: x[1], reverse=True)
-    # show
-    print('> {}'.format(sentence))
-    # top sort is best fit:
+
     for i, sc in enumerate(sorted_sentence_combinations[0:1]):
-        solid_match.append(commands[sc[0]])
-        print('  {:.4f} {}'.format(sc[1][0], commands[sc[0]]))
-
-
-# Find possible variables
-for i, command in enumerate(solid_match):
+      if sc[1][0] > .5:
+        return self.commands[sc[0]]
+      else:
+        return None
+  
+  def findVariables(self, solid_match, matched_command):
     var = None
-    if "[type]" in command:
-        if "code" in input_sentences[i]:
+    if "[type]" in matched_command:
+        if "code" in solid_match:
           var = "code"
-        elif "text" in input_sentences[i]:
+        elif "text" in solid_match:
           var = "text"
         else:
           var = "Unkown"
@@ -77,10 +52,10 @@ for i, command in enumerate(solid_match):
           # 1. Ask user to provide a correct input for this command
           # 2. Call Speech to Text command
           # 3. Preprocess text for passing to executing that command
-    elif "[up/down]" in command:
-      if "up" in input_sentences[i]:
+    elif "[up/down]" in matched_command:
+      if "up" in solid_match:
         var = "up"
-      elif "down" in input_sentences[i]:
+      elif "down" in solid_match:
         var = "down"
       else:
         var = "Unknown"
@@ -89,10 +64,5 @@ for i, command in enumerate(solid_match):
           # 2. Call Speech to Text command
           # 3. Preprocess text for passing to executing that command
     # TODO: get optional{} vars from commands, this will depend on most likely command, and can be extrapolated to more simple function calls.
-    print(f"current command: {input_sentences[i]}\n intepreted as: {command}\n has var: {var}")
-
-# Execute command
-
-    # TODO: run that command with passed variables.
-
-
+    print(f"current command: {solid_match}\n intepreted as: {matched_command}\n has var: {var}")
+    # TODO: run that command.
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 # Main.py where we run our program, and keep control of all called processes.
 
 import keyboard
-# from TTC import ClosestCommand
+from TTC import ClosestCommand
 
 if __name__ == "__main__":
+    # initiate Text to Commands class
+    ttc = ClosestCommand.TextToCommands()
+    
     response = ""
     while True:
         if keyboard.is_pressed('ctrl+r'):
             print("listening")
             response = "run code"
-            # Run Speech to Text
+            print(ttc.findMatch(response))
         elif response != "":
             print("Run TTC")
         else:
