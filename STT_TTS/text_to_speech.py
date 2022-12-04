787ec69207ba93a5848cf826f997e5db9b483d22
Matthew Shane Blair <65378435+ShaneBlair@users.noreply.github.com>
2022-12-03 22:27:56 -0600

    Update text_to_speech.py

--- STT_TTS/text_to_speech.py
+++ STT_TTS/text_to_speech.py
@@ -1,65 +1,51 @@
-from gtts import gTTS
+import wx
 import os
-import pyttsx3
 import accessible_output2.outputs.auto
 ao_output = accessible_output2.outputs.auto.Auto()
-# Run Program: python -m speech_recognition
-import speech_recognition as sr
-import pyttsx3
+import sys
+from stt_tts import SpeechToText
 
-#TODO: Use accessible_output2 to recognize if user has a screen reader, use their voice and rate first
-# If not, use the default. Ask Thomas if he can help with this.
+###############################################################################
+class MyFrame(wx.Frame):
+    def __init__(self, parent, title):
+        super(MyFrame, self).__init__(parent, title=title, size = (700, 600))
+        self.myGUI()
+        panel = wx.Panel(self)
 
-# Initialize the recognizer
-r = sr.Recognizer()
+        self.btn = wx.Button(panel,-1,"Enter voice command: ", pos =(200, 250), size = (300,40))
+        self.btn.Bind(wx.EVT_BUTTON, self.OnClicked)
 
-# Function to convert text to speech
-def SpeakText(command):
 
-    # Initialize the engine
-    engine = pyttsx3.init()
-    engine.say(command)
-    engine.runAndWait()
+    def myGUI(self):
+        menu_bar = wx.MenuBar()
+        file_button = wx.Menu()
+        
+        #save_item = file_button.Append(wx.ID_EXIT, 'Save File','Saving ...')
+        #load_item = file_button.Append(wx.ID_EXIT, 'Load File','Loading ...')
+        exit_item = file_button.Append(wx.ID_EXIT, 'Exit','Exiting ...')
 
-    # Path to Google Chrome
-    path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
+        menu_bar.Append(file_button, 'File')
 
-# Use the microphone as source for input
-with sr.Microphone() as source2:
+        self.SetMenuBar(menu_bar)
+        self.Bind(wx.EVT_MENU, self.Quit, exit_item)
 
-    # Wait for the recognizer to adjust the energy threshold based on the background noise
-    r.adjust_for_ambient_noise(source2, duration=0.2)
+    def Quit(self, e):
+        self.Close()
 
-    # Listens for the user's input
-    audio2 = r.listen(source2)
+    def OnClicked(self, event): 
+        btn = event.GetEventObject().GetLabel() 
+        print(("Speech to Text button pressed successfully."),btn)
+        SpeechToText()
 
-    # Using google to recognize audio
-    MyText = r.recognize_google(audio2)
+    def OnToggle(self,event): 
+      state = event.GetEventObject().GetValue()
 
-    if MyText == "open website":
-        print("You have said: " + MyText)
-        print("What website would you like?")
+class MyApp(wx.App):
+    def OnInit(self):
+        self.frame = MyFrame(parent=None, title="Accessible Google Colaboratory")
+        self.frame.Show()
 
-        r.adjust_for_ambient_noise(source2, duration=0.2)
-        audio2 = r.listen(source2)
-        MyText = r.recognize_google(audio2)
+        return True
 
-    else:
-        MyText = MyText.lower()
-
-        print("Did you say " +MyText)
-        SpeakText(MyText)
-
-# Below code is the proof of concept
-#sample_string = 'North Texas.'  # Sample string dataset
-
-#speech_output_2 = gTTS(sample_string, lang = 'en', slow = True)
-#speech_output_2.save("test_audio.mp3")
-#os.system("test_audio.mp3")
-#Audio("test_audio.mp3", autoplay=True)
-
-# Once I receive the string from Solomon's STT, pass it into gTTS
-print(MyText)
-speech_output = gTTS(MyText, lang = 'en', slow = True)
-speech_output.save("audio.mp3")
-os.system("audio.mp3")
+app = MyApp()
+app.MainLoop()
